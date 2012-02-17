a66604a3e9350a742532db9c29d9d3a92e3cfc5f
tedsuo <ted@radicaldesigns.org>
2012-02-17 11:42:58 -0800

    foo

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-STYLES_LESS = ./less/styles.less
+STYLES_LESS = ./less/styles_v2.less
 BOOTSTRAP_LESS = ./bootstrap/less/bootstrap.less
 BOOTSTRAP_RESPONSIVE_LESS = ./bootstrap/less/responsive.less
 LESS_COMPRESSOR ?= `which lessc`
@@ -15,7 +15,7 @@ css:
 	lessc --compress ${BOOTSTRAP_LESS} > bootstrap/css/bootstrap.min.css
 	lessc ${BOOTSTRAP_RESPONSIVE_LESS} > bootstrap/css/bootstrap-responsive.css
 	lessc --compress ${BOOTSTRAP_RESPONSIVE_LESS} > bootstrap/css/bootstrap-responsive.min.css
-	lessc ${STYLES_LESS} > css/styles.css
-	lessc --compress ${STYLES_LESS} > css/styles.min.css
+	lessc ${STYLES_LESS} > css/styles_v2.css
+	lessc --compress ${STYLES_LESS} > css/styles_v2.min.css
 
 .PHONY: all css
--- build/index.js
+++ build/index.js
@@ -1,31 +1,46 @@
 #!/usr/bin/env node
 var hogan = require('hogan.js')
   , fs    = require('fs')
+  , less  = require('less');
 
-var layout, pages
+var layout, pages;
 
 // compile layout template
-layout = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8')
-layout = hogan.compile(layout, { sectionTags: [{o:'_i', c:'i'}] })
+layout = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8');
+layout = hogan.compile(layout, { sectionTags: [{o:'_i', c:'i'}] });
 
 // retrieve pages
-pages = fs.readdirSync(__dirname + '/../templates/pages')
+fs.readdir(__dirname + '/../templates/pages',function(err,pages){
+  pages.forEach(function (name) {
 
-// iterate over pages
-pages.forEach(function (name) {
+    if (!name.match(/\.mustache$/)) return;
 
-  if (!name.match(/\.mustache$/)) return
+    var version = name.charAt(name.length-10),
+        context = {};
 
-  var page = fs.readFileSync(__dirname  + '/../templates/pages/' + name, 'utf-8')
-    , context = {}
+    fs.readFile(__dirname  + '/../templates/pages/' + name, 'utf-8', function(err,page){
+      context[name.replace(/\.mustache$/, '')] = 'active';
+      context._i = true;
+      page = hogan.compile(page, { sectionTags: [{o:'_i', c:'i'}] });
+      page = layout.render(context, {
+        body: page,
+        version: version
+      })
+      fs.writeFileSync(__dirname + '/../' + name.replace(/mustache$/, 'html'), page, 'utf-8');
+    })
+  });
+});
 
-  context[name.replace(/\.mustache$/, '')] = 'active'
-  context._i = true
 
-  page = hogan.compile(page, { sectionTags: [{o:'_i', c:'i'}] })
-  page = layout.render(context, {
-    body: page
-  })
+// retrieve less files
+fs.readdir(__dirname + '/../less', function(err,less_files){
+  less_files.forEach(function (name) {
 
-  fs.writeFileSync(__dirname + '/../' + name.replace(/mustache$/, 'html'), page, 'utf-8')
-})
+    if(!name.match(/\.less$/)) return;
+    if(name.match(/variables\.less$/)) return;
+
+    fs.readFile(__dirname + '/../less/'+name,'utf-8',function(err,data){
+      
+    });
+  });
+});
